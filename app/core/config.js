73962d9c21d51a0ddc00df4d9426e05225f76825
liang <guoliang1222@gmail.com>
2017-02-15 21:58:24 +0200

    change to production api

--- app/core/config.js
+++ app/core/config.js
@@ -1,6 +1,6 @@
 const config = {
-	api: 'http://chat.loveyoung.me:9080/api/v1',
-	ws: 'http://chat.loveyoung.me:9080'
+	api: 'http://chat.loveyoung.me/api/v1',
+	ws: 'http://chat.loveyoung.me'
 };
 
 export default config;
